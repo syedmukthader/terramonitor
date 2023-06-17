850ba7cdafec9f5feb32575e34c968dcbca07619
Syed-Mukthader <76219785+syedmukthader@users.noreply.github.com>
2023-06-17 22:21:31 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,47 +36,56 @@ resource "aws_iam_instance_profile" "deploy" {
   role = "EC2-ECR"
 }
 resource "aws_security_group" "mainsg" {  
-egress = [
-  {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 0
+  ingress = [
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]  
       ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocal         = "-1"
-      security_groups  = []
-      self             = false
-      to_port          = 0
-  }
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    },
+  {
+      description      = "HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]  
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = false      
+  },
+
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]  
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = false      
+    }
   ]
 
-  ingress = [
-  {
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-    },
+
+  egress = [
     {
-    description      = "for all outgoing traffics"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      from_port        = 22
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      protocol         = "tcp"
-      security_groups  = []
-      self             = false
-      to_port          = 22
-  }
+      description      = "for all outgoing traffics"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = []
+      security_groups = []
+      self = false
+    }
   ]
 }
 
