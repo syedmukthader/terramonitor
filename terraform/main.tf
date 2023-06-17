54bf031c53d1e369dfbd6e83da9e2ad0341998e3
syedmukthader <76219785+syedmukthader@users.noreply.github.com>
2023-06-17 14:20:15 +0100

    v1

---
+++ terraform/main.tf
@@ -0,0 +1,87 @@
+terraform {
+required_providers {
+  aws = {
+    source = "hashicorp/aws"
+    version = "~>4.0"
+  }
+}
+ backend "s3"{
+   key = "aws/ec2-deploy/terraform.tfstate"
+ }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "server" {
+  ami = "ami-0989fb15ce71ba39e"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.mainsg.id]
+  iam_instance_profile = ""
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
+  tags = {
+    "name" = "DeployVM"
+  }
+}
+resource "aws_iam_instance_profile" "deploy" {
+  name = "deploy"
+  role = "EC2-ECR"
+}
+
+  resource "aws_security_group" "mainsg" {
+  egress = [
+    {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = ""
+    from_port = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids = []
+    protocal = "-1"
+    security_groups = []
+    self = false
+    to_port = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks = ["0.0.0.0/0"]
+      description = ""
+      from_port = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      protocol = "tcp"
+      security_groups = []
+      self = false
+      to_port = 22
+    },
+    {
+      cidr_blocks = ["0.0.0.0/0"]
+      description = ""
+      from_port = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      protocol = "tcp"
+      security_groups = []
+      self = false
+      to_port = 22
+    }
+    ]
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name = var.key_name
+  public_key = var.public_key
+}
+
+output "instance_public_ip" {
+  value = aws_instance.server.public_ip
+  sensitive = true
+}
